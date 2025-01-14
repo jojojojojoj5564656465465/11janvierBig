839e1c95bcb4aad3ec973fb2917fb3a3b31bb660
jonathan <bcbg92@hotmail.com>
2025-01-15 00:52:57 +0100

    all good carrousel

--- src/components/starter/carrousel/card.css.ts
+++ src/components/starter/carrousel/card.css.ts
@@ -1,6 +1,11 @@
-import { media } from "@/styles/ThemeContract.css";
+import { media, theme } from "@/styles/ThemeContract.css";
 import { fluid } from "~/styles/utils";
-import { assignVars, createThemeContract, style } from "@vanilla-extract/css";
+import {
+	assignVars,
+	createThemeContract,
+	globalStyle,
+	style,
+} from "@vanilla-extract/css";
 
 /* =====================================
 =               theme contract         =
@@ -58,94 +63,61 @@ export const varsColor = createThemeContract({
 });
 
 export const ColorTheme = style({
-  vars: assignVars(varsColor, {
-    colorCard: {
-      title: "#DD7373",
-      description: "#3B3561",
-      backgroundColor: "#D1D1D1",
-      btnColor: "#51A3A3",
-    },
-  }),
-  "@media": {
-    [media.dark]: {
-      vars: assignVars(varsColor, {
-        colorCard: {
-          title: "#F0EDEE",
-          description: "#EEC8E0",
-          backgroundColor: "#0A090C",
-          btnColor: "#90DDF0",
-        },
-      }),
-    },
-  },
+	vars: assignVars(varsColor, {
+		colorCard: {
+			title: theme.color.azure,
+			description: "#3B3561",
+			backgroundColor: "#D1D1D1",
+			btnColor: theme.color.green,
+		},
+	}),
+	"@media": {
+		[media.dark]: {
+			vars: assignVars(varsColor, {
+				colorCard: {
+					title: "#F0EDEE",
+					description: "oklch(80% 0.1285 106.94)",
+					backgroundColor: "oklch(25.88% 0.0564 243.53)",
+					btnColor: "#90DDF0",
+				},
+			}),
+		},
+	},
 });
 
 export const wrapperCard = style([
-  {
-    display: "flex",
-    flexDirection: "column",
-    width: "230px",
-    maxInlineSize: `calc((100% - (${vars.space.items} - 1) * ${vars.space.gap}) / ${vars.space.items})`,
+	{
+		// display: "flex",
 
-    minWidth: "200px",
-    height: "280px",
-    maxHeight: "330px",
-    backgroundColor: varsColor.colorCard.backgroundColor,
-    borderRadius: "10px",
-    boxShadow:
-      "0px 10px 12px rgba(0,0,0,0.08) , -4px -4px 12px rgba(0,0,0,0.08)",
-    overflow: "hidden",
-    transition: "all 0.3s",
-    cursor: "pointer",
-    boxSizing: "border-box",
-    padding: "10px",
-    ":hover": {
-      transform: "translateY(-10px)",
-      boxShadow:
-        "0px 20px 20px rgba(0,0,0,0.1) , -4px -4px 12px rgba(0,0,0,0.08)",
-    },
-  },
-  ColorTheme,
+		// flexDirection: "column",
+		display: "grid",
+		gridColumn: "1fr",
+		gridTemplateRows: "auto min-content 1fr 1fr",
+		//width: "23px",
+		rowGap: 5,
+		maxInlineSize: `calc((100% - (${vars.space.items} - 1) * ${vars.space.gap}) / ${vars.space.items})`,
+
+		minWidth: "200px",
+		height: "220px",
+		maxHeight: "330px",
+		backgroundColor: varsColor.colorCard.backgroundColor,
+		borderRadius: "10px",
+		boxShadow:
+			"0px 10px 12px rgba(0,0,0,0.08) , -4px -4px 12px rgba(0,0,0,0.08)",
+		overflow: "hidden",
+		transition: "all 0.3s",
+		cursor: "progress",
+		boxSizing: "border-box",
+		padding: "10px",
+		":hover": {
+			transform: "translateY(-10px)",
+			boxShadow:
+				"0px 20px 20px rgba(0,0,0,0.1) , -4px -4px 12px rgba(0,0,0,0.08)",
+		},
+	},
+	ColorTheme,
 ]);
 
-export const Btn = style({
-  fontSize: "15px",
-  fontFamily:
-    "Lucida Sans , Lucida Sans Regular , Lucida Grande , Lucida Sans Unicode , Geneva , Verdana , sans-serif",
-  color: varsColor.colorCard.title,
-  cursor: "pointer",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: varsColor.colorCard.btnColor,
-  width: "25px",
-  height: "25px",
-  maxHeight: "25px",
-  borderRadius: "10px",
-  overflow: "hidden",
-  transition: "all ease-in-out 0.5s",
-  gap: "1px",
-  boxSizing: "border-box",
-  paddingLeft: "5px",
-  marginTop: "8px",
-  ":hover": {
-    boxShadow: "0px 10px 20px rgba(0,0,0,0.06)",
-    width: "100%",
-    borderRadius: "8px",
-    height: "30px",
-    gap: "10px",
-    padding: "0",
-  },
-  selectors: {
-    [`${wrapperCard}:hover > &`]: {
-      opacity: "1",
-      fontSize: "15px",
-      width: "100%",
-      height: "30px",
-      gap: "10px",
-    },
-  },
-});
 
 export const price = style([
   {
@@ -184,55 +156,100 @@ export const Title = style({
   lineClamp: "1",
 });
 
-export const videoIcon = style({
-  fontSize: fluid(40, 60),
+
+export const buttonBlue = style({
+	fontFamily:
+		"Lucida Sans , Lucida Sans Regular , Lucida Grande , Lucida Sans Unicode , Geneva , Verdana , sans-serif",
+	color: varsColor.colorCard.title,
+	cursor: "pointer",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	backgroundColor: varsColor.colorCard.btnColor,
+	width: "25px",
+	height: "25px",
+	maxHeight: "35px",
+	// borderRadius: "10px",
+	overflow: "hidden",
+	transition: "all ease-in-out 0.5s",
+	gap: "1px",
+	boxSizing: "border-box",
+
+	marginTop: "8px",
+	clipPath: "polygon(0 0, 0% 100%, 100% 45%)",
+	":hover": {
+		clipPath: "inset(0 0 0 0)",
+		boxShadow: "0px 10px 20px rgba(0,0,0,0.06)",
+		width: "100%",
+		borderRadius: "8px",
+		height: "30px",
+		
+		padding: "0",
+	},
+	selectors: {
+		[`${wrapperCard}:hover &`]: {
+			clipPath: "inset(0 0 0 0)",
+			boxShadow: "0px 10px 20px rgba(0,0,0,0.06)",
+			width: "100%",
+			borderRadius: "8px",
+			height: "30px",
+			gap: "10px",
+			padding: "0",
+			color: "black",
+		},
+	},
 });
 
-export const BtnText = style({
-  opacity: "0",
-  fontSize: "1px",
-  fontWeight: "500",
-  transition: "all ease-in-out 0.5s",
-  selectors: {
-    [`${Btn}:hover > &`]: {
-      opacity: "1",
-      fontSize: "15px",
-    },
-
-    [`${wrapperCard}:hover > &`]: {
-      opacity: "1",
-      fontSize: "15px",
-    },
-  },
+export const buttonBlue_Text = style({
+	opacity: "0",
+	fontSize: "1px",
+	fontWeight: "500",
+	transition: "all ease-in-out 0.8s",
+	selectors: {
+		[`${wrapperCard}:hover  &`]: {
+			opacity: "1",
+			fontSize: "15px",
+		},
+		[`${buttonBlue}:hover &`]: {
+			opacity: "1",
+			fontSize: "15px",
+		},
+	},
 });
 
+
+
+
+/**
+ * Description images
+ *MARK: IMAGES
+ * @type {*}
+ */
 export const imageHoverContract = createThemeContract({
-  image: null,
-  gif: null,
+	image: null,
+	gif: null,
 });
 const ImageContainer = style([
-  {
-    width: "100%",
-    height: "64%",
-    borderRadius: "10px",
-    marginBottom: "12px",
-    overflow: "hidden",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    //aspectRatio: '16 / 9',
-  },
+	{
+		width: "100%",
+		borderRadius: "10px",
+		overflow: "hidden",
+		backgroundSize: "cover",
+		backgroundRepeat: "no-repeat",
+		aspectRatio: "16 / 9",
+	},
 ]);
 export const hoverImg = style([
-  ImageContainer,
-  {
-    backgroundImage: imageHoverContract.image,
-    selectors: {
-      [`${wrapperCard}:hover > &`]: {
-        backgroundImage: imageHoverContract.gif,
-      },
-    },
-  },
+	ImageContainer,
+	{
+		backgroundImage: imageHoverContract.image,
+		selectors: {
+			[`${wrapperCard}:hover > &`]: {
+				backgroundImage: imageHoverContract.gif,
+			},
+			[`${wrapperCard}:focus > &`]: {
+				backgroundImage: imageHoverContract.gif,
+			},
+		},
+	},
 ]);
--- src/components/starter/carrousel/index.tsx
+++ src/components/starter/carrousel/index.tsx
@@ -51,29 +51,14 @@ const Card = component$<CardProps>((props) => {
         class={card.hoverImg}
         style={assignInlineVars(card.imageHoverContract, {
           gif: `url(${props.Gif})` || `url(${props.Image})`,
-          image: `url(${props.Image})`,
+          image: `url(${props.Image})` || `url(${props.Gif})`,
         })}
       ></div>
       <p class={card.Title}>{props.Category}</p>
       <p class={card.price}>{props.Price}</p>
       <a href={props.Link}>
-        <div class={card.Btn}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            stroke="currentColor"
-            stroke-width="0"
-            viewBox="0 0 384 512"
-          >
-            <path
-              stroke="none"
-              d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80v352c0 17.4 9.4 33.4 24.5 41.9S58.2 482 73 473l288-176c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"
-            />
-            <title>PLAY</title>
-          </svg>
-          <span class={card.BtnText}>Voir la Visite</span>
+        <div class={card.buttonBlue}>
+          <span class={card.buttonBlue_Text}>Voir la Visite</span>
         </div>
       </a>
     </section>
