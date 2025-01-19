1d93ef26aa071a554369221f90433ace4776cdbd
jonathan <bcbg92@hotmail.com>
2025-01-19 18:30:16 +0100

    update

--- src/routes/propUp/index.css.ts
+++ src/routes/propUp/index.css.ts
@@ -9,23 +9,31 @@ import { globalStyleTag } from "~/styles/utils/globalStyleTag";
 export const s = style({
 	margin: 25,
 	maxWidth: 650,
-	":hover": {
-		maxWidth: "100%",
-	},
-
+	backgroundColor: "orange",
+	aspectRatio: "1",
+	display: "grid",
+	gridTemplateColumns: "1fr",
+	gridTemplateRows: "1fr",
+	alignItems: "end",
+	//justifyItems: "end",
 });
 
-globalStyleTag(s, {
-	h2: {
-		color: "green",
-		backgroundColor: "red",
-	},
-	h5: {
-		color: "pink",
-	},
-	p: { backgroundColor: "orange", fontSize: "6rem" },
-	h6: {
-		backgroundColor: "white",
-		fontSize: "4rem",
-	},
+export const baseImage = style({
+	width: 500,
+
+	gridRow: "1 / span 2",
+	gridColumn: "1 / span 2",
+	borderWidth: 10,
+	borderColor: "red",
 });
+
+const palette = {
+	primary: { bg: "blue", h: 650 },
+	secondary: { bg: "green", h: 230 },
+	ternary: { bg: "yellow", h: 100 },
+}as const ;
+
+export const variant = styleVariants(palette, ({ bg, h }) => [
+	baseImage,
+	{ background: bg, height: h },
+]);
--- src/routes/propUp/index.tsx
+++ src/routes/propUp/index.tsx
@@ -1,17 +1,15 @@
 import { component$ } from "@builder.io/qwik";
-import {s} from "./index.css"
+import { s, variant } from "./index.css";
 import Image from "./public/images/carrousel/images/spa.webp?&jsx";
 
 export default component$(() => {
   return (
-      <>
+    <>
       <h2>hello world</h2>
-    <div class={s}>
-      <Image/>
-      <h5>h5 mon ami</h5>
-      <h6>h6 mon ami</h6>
-      <p>p mon ami</p>
-    </div>
-      </>
+      <section class={s}>
+        <div class={variant.primary}/>
+        <div class={variant.ternary}/>
+      </section>
+    </>
   );
 });
